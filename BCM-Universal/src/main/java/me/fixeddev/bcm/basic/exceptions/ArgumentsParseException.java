8c71252957772dc363402c0cfb9487fe6be1d3ac
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-20 12:08:46 -0500

    Added constructors with a message argument This was added because otherwise i couldn't send a message when a ArgumentParse failed, i only could send the argument and the argument type, this adds a lot of flexibility to the command manager
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- BCM-Universal/src/main/java/me/fixeddev/bcm/basic/exceptions/ArgumentsParseException.java
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/basic/exceptions/ArgumentsParseException.java
@@ -4,4 +4,16 @@ public class ArgumentsParseException extends Exception {
     public ArgumentsParseException(String argument, Class<?> type) {
         super("Failed to parse argument: " + argument + " type: " + type.getName());
     }
+
+    public ArgumentsParseException(String argument, Class<?> type, String message) {
+        super("Failed to parse argument: " + argument + " type: " + type.getName() + " error message: " + message);
+    }
+
+    public ArgumentsParseException(String argument, Class<?> type, Class<?> annotationType) {
+        super("Failed to parse argument: " + argument + " type: " + type.getName() + " annotated with: " + annotationType.getName());
+    }
+
+    public ArgumentsParseException(String argument, Class<?> type, Class<?> annotationType, String message) {
+        super("Failed to parse argument: " + argument + " type: " + type.getName() + " annotated with: " + annotationType.getName() + " error message: " + message);
+    }
 }
