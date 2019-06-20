1f0d333c3b5f262b7da3c8e1669580bef56db774
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-20 12:15:02 -0500

    The StringParameterProvider now is divided in 2 The JoinedString annotation before was handled directly by the StringParameterProvider, now with the changes in the api that can be handled by a different ParameterProvider. In consequence we get a cleaner code
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- BCM-Universal/src/main/java/me/fixeddev/bcm/parametric/providers/StringParameterProvider.java
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/parametric/providers/StringParameterProvider.java
@@ -4,50 +4,13 @@ import me.fixeddev.bcm.basic.ArgumentStack;
 import me.fixeddev.bcm.basic.Namespace;
 import me.fixeddev.bcm.basic.exceptions.NoMoreArgumentsException;
 import me.fixeddev.bcm.parametric.ParameterProvider;
-import me.fixeddev.bcm.parametric.annotation.JoinedString;
 
 import java.lang.annotation.Annotation;
-import java.util.List;
-import java.util.Optional;
-import java.util.StringJoiner;
 
 public class StringParameterProvider implements ParameterProvider<String> {
     @Override
-    public String transformParameter(ArgumentStack arguments, Namespace namespace, List<Annotation> modifiers, String defaultValue) throws NoMoreArgumentsException {
-        Optional<JoinedString> found = Optional.empty();
-
-        for (Annotation modifier : modifiers) {
-            if (modifier == null || modifier.annotationType() != JoinedString.class) {
-                continue;
-            }
-
-            found = Optional.of((JoinedString) modifier);
-        }
-
+    public String transformParameter(ArgumentStack arguments, Namespace namespace, Annotation annotation, String defaultValue) throws NoMoreArgumentsException {
         try {
-            if (found.isPresent()) {
-                StringJoiner joiner = new StringJoiner(" ");
-
-                int consumedArgs = 1;
-
-                while (arguments.hasNext()
-                        && (found.get().value() == -1 || consumedArgs <= found.get().value())) {
-                    joiner.add(arguments.next());
-
-                    consumedArgs++;
-                }
-
-                String returnValue = joiner.toString();
-
-                if ((defaultValue != null && !defaultValue.trim().isEmpty())
-                        && returnValue.trim().isEmpty()) {
-                    return defaultValue;
-                }
-
-                return returnValue.trim();
-            }
-
-
             return arguments.next();
         } catch (NoMoreArgumentsException e) {
             if (defaultValue == null || defaultValue.isEmpty()) {
