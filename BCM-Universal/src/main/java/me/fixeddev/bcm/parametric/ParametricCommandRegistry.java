1da665c618274f823c3efc982b97c8e521705e1a
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-20 11:31:26 -0500

    Modified some methods to add the support for annotations
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- BCM-Universal/src/main/java/me/fixeddev/bcm/parametric/ParametricCommandRegistry.java
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/parametric/ParametricCommandRegistry.java
@@ -1,25 +1,33 @@
 package me.fixeddev.bcm.parametric;
 
 import me.fixeddev.bcm.basic.CommandRegistry;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 public interface ParametricCommandRegistry extends CommandRegistry {
     void registerCommandClass(CommandClass commandClass);
 
-    Map<Class<?>, ParameterProvider> getRegisteredParameterTransformers();
+    @NotNull
+    Map<Class<?>, Map<Class<?>, ParameterProvider>> getRegisteredParameterTransformers();
 
-    <T> ParameterProvider<T> getParameterTransformer(Class<T> clazz);
+    @Nullable
+    <T> ParameterProvider<T> getParameterTransformer(@NotNull Class<T> clazz, @Nullable Class<?> annotationType);
 
-    default <T> void registerParameterTransfomer(Class<T> clazz, ParameterProvider<T> parameterProvider) {
+    <T> void registerParameterTransformer(@NotNull Class<T> clazz, @Nullable Class<?> annotationType, @NotNull ParameterProvider<T> parameterProvider);
+
+    <T> boolean hasRegisteredTransformer(@NotNull Class<T> clazz, @Nullable Class<?> annotationType);
+
+    default <T> ParameterProvider<T> getParameterTransformer(@NotNull Class<T> clazz) {
+        return getParameterTransformer(clazz, null);
+    }
+
+    default <T> void registerParameterTransfomer(@NotNull Class<T> clazz, @NotNull ParameterProvider<T> parameterProvider) {
         registerParameterTransformer(clazz, null, parameterProvider);
     }
 
-    <T> void registerParameterTransformer(Class<T> clazz, Class<?> annotationType, ParameterProvider<T> parameterProvider);
-
-    default <T> boolean hasRegisteredTransformer(Class<T> clazz) {
+    default <T> boolean hasRegisteredTransformer(@NotNull Class<T> clazz) {
         return hasRegisteredTransformer(clazz, null);
     }
-
-    <T> boolean hasRegisteredTransformer(Class<T> clazz, Class<?> annotationType);
 }
