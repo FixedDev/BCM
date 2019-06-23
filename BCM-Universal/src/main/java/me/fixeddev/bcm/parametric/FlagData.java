5adfe3faf2a2237cd4332fad765b03990b9e60b9
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-23 17:38:05 -0500

    Create a ParameterData exclusive for Flags
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

---
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/parametric/FlagData.java
@@ -0,0 +1,34 @@
+package me.fixeddev.bcm.parametric;
+
+import me.fixeddev.bcm.parametric.annotation.Flag;
+import me.fixeddev.bcm.parametric.annotation.Optional;
+import me.fixeddev.bcm.parametric.annotation.Parameter;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+class FlagData extends ParameterData {
+    public FlagData(Flag parameter, Optional optional, List<Annotation> modifiers) {
+        super(boolean.class, getFromFlag(parameter), optional, modifiers);
+    }
+
+    static Parameter getFromFlag(Flag flag){
+        return new Parameter() {
+
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return Parameter.class;
+            }
+
+            @Override
+            public String value() {
+                return flag.value() + "";
+            }
+
+            @Override
+            public boolean isFlag() {
+                return true;
+            }
+        };
+    }
+}
