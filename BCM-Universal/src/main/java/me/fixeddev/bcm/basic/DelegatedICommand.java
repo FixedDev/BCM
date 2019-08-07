fe220e37af9f8c8405cf13e0697bf0309ba5514e
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-08-07 00:30:15 -0500

    Revert "Check for a null delegate"
    This reverts commit 49da7367
    
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- BCM-Universal/src/main/java/me/fixeddev/bcm/basic/DelegatedICommand.java
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/basic/DelegatedICommand.java
@@ -5,30 +5,17 @@ import me.fixeddev.bcm.basic.exceptions.CommandException;
 import me.fixeddev.bcm.basic.exceptions.NoMoreArgumentsException;
 import me.fixeddev.bcm.basic.exceptions.NoPermissionsException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface DelegatedICommand extends ICommand, DelegatedCommand<ICommand> {
 
     @Override
     default boolean run(Namespace namespace, ArgumentArray arguments) throws CommandException, NoPermissionsException, NoMoreArgumentsException, ArgumentsParseException {
-        ICommand delegate = getDelegate();
-
-        if(delegate == null){
-            return true;
-        }
-
-        return delegate.run(namespace, arguments);
+        return getDelegate().run(namespace, arguments);
     }
 
     @Override
     default List<String> getSuggestions(Namespace namespace, ArgumentArray arguments) throws CommandException, NoMoreArgumentsException {
-        ICommand delegate = getDelegate();
-
-        if(delegate == null){
-            return new ArrayList<>();
-        }
-
-        return delegate.getSuggestions(namespace, arguments);
+        return getDelegate().getSuggestions(namespace, arguments);
     }
 }
