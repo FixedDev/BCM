cb2e9c52d7ba7fa0d8ceb7f8e207e45ef37e75e0
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-08-07 00:30:24 -0500

    Revert "Whoops, missed to override the getSuggestions method"
    This reverts commit 16b2b02c
    
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- BCM-Universal/src/main/java/me/fixeddev/bcm/basic/DelegatedICommand.java
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/basic/DelegatedICommand.java
@@ -5,17 +5,10 @@ import me.fixeddev.bcm.basic.exceptions.CommandException;
 import me.fixeddev.bcm.basic.exceptions.NoMoreArgumentsException;
 import me.fixeddev.bcm.basic.exceptions.NoPermissionsException;
 
-import java.util.List;
-
 public interface DelegatedICommand extends ICommand, DelegatedCommand<ICommand> {
 
     @Override
     default boolean run(Namespace namespace, ArgumentArray arguments) throws CommandException, NoPermissionsException, NoMoreArgumentsException, ArgumentsParseException {
         return getDelegate().run(namespace, arguments);
-    }
-
-    @Override
-    default List<String> getSuggestions(Namespace namespace, ArgumentArray arguments) throws CommandException, NoMoreArgumentsException {
-        return getDelegate().getSuggestions(namespace, arguments);
     }
 }
