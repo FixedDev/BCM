16b2b02c8c68bf612fe2c8bf9b211579f50fa969
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-08-07 00:26:35 -0500

    Whoops, missed to override the getSuggestions method
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- BCM-Universal/src/main/java/me/fixeddev/bcm/DelegatedAdvancedCommand.java
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/DelegatedAdvancedCommand.java
@@ -1,14 +1,23 @@
 package me.fixeddev.bcm;
 
+import me.fixeddev.bcm.basic.ArgumentArray;
 import me.fixeddev.bcm.basic.DelegatedCommand;
+import me.fixeddev.bcm.basic.Namespace;
 import me.fixeddev.bcm.basic.exceptions.ArgumentsParseException;
 import me.fixeddev.bcm.basic.exceptions.CommandException;
 import me.fixeddev.bcm.basic.exceptions.NoMoreArgumentsException;
 import me.fixeddev.bcm.basic.exceptions.NoPermissionsException;
 
+import java.util.List;
+
 public interface DelegatedAdvancedCommand extends AdvancedCommand, DelegatedCommand<AdvancedCommand> {
     @Override
     default boolean execute(CommandContext context) throws CommandException, NoPermissionsException, NoMoreArgumentsException, ArgumentsParseException {
         return getDelegate().execute(context);
+    }
+
+    @Override
+    default List<String> getSuggestions(Namespace namespace, ArgumentArray arguments) throws CommandException, NoMoreArgumentsException {
+        return getDelegate().getSuggestions(namespace, arguments);
     }
 }
