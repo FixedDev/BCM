ddb579d3371f93c59625754320ff9b0ecff0727f
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-08-07 23:54:12 -0500

    Created a registry for ParameterProviders At the moment it offers the same functionality that the old ParametricCommandRegistry
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

---
+++ BCM-Universal/src/main/java/me/fixeddev/bcm/parametric/ParameterProviderRegistry.java
@@ -0,0 +1,30 @@
+package me.fixeddev.bcm.parametric;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+public interface ParameterProviderRegistry {
+    @NotNull
+    Map<Class<?>, Map<Class<?>, ParameterProvider>> getRegisteredParameterTransformers();
+
+    @Nullable
+    <T> ParameterProvider<T> getParameterTransformer(@NotNull Class<T> clazz, @Nullable Class<?> annotationType);
+
+    <T> void registerParameterTransformer(@NotNull Class<T> clazz, @Nullable Class<?> annotationType, @NotNull ParameterProvider<T> parameterProvider);
+
+    <T> boolean hasRegisteredTransformer(@NotNull Class<T> clazz, @Nullable Class<?> annotationType);
+
+    default <T> ParameterProvider<T> getParameterTransformer(@NotNull Class<T> clazz) {
+        return getParameterTransformer(clazz, null);
+    }
+
+    default <T> void registerParameterTransfomer(@NotNull Class<T> clazz, @NotNull ParameterProvider<T> parameterProvider) {
+        registerParameterTransformer(clazz, null, parameterProvider);
+    }
+
+    default <T> boolean hasRegisteredTransformer(@NotNull Class<T> clazz) {
+        return hasRegisteredTransformer(clazz, null);
+    }
+}
